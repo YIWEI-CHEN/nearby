0cc1c59880c5ad19b6b1cdf710ba7ff90b0befbb
ahxt <han@tamu.edu>
2019-10-27 00:24:30 -0500

    update google login

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -39,7 +39,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+
+    #ahxt
     "hello",
+    'django.contrib.sites', # new
+    'allauth', # new
+    'allauth.account', # new
+    'allauth.socialaccount', # new
+    'allauth.socialaccount.providers.github', # new
+    'allauth.socialaccount.providers.google', # new
 
 ]
 
@@ -116,5 +125,16 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+
+
+# ahxt
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = 'index'
 
 django_heroku.settings(locals())
