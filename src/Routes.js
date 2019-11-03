c37a766605125978113a1a38ba3711f15e6ba09e
Yi-Wei Chen <feberium@gmail.com>
2019-11-03 01:12:42 -0500

    add sign-in/sign-up frontend

---
+++ src/Routes.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Switch, Redirect } from 'react-router-dom';
+
+import { RouteWithLayout } from './components';
+import { Minimal as MinimalLayout } from './layouts';
+
+import {
+  SignUp as SignUpView,
+  SignIn as SignInView,
+  NotFound as NotFoundView
+} from './views';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Redirect
+        exact
+        from="/"
+        to="/sign-in"
+      />
+      <RouteWithLayout
+        component={SignUpView}
+        exact
+        layout={MinimalLayout}
+        path="/sign-up"
+      />
+      <RouteWithLayout
+        component={SignInView}
+        exact
+        layout={MinimalLayout}
+        path="/sign-in"
+      />
+      <RouteWithLayout
+        component={NotFoundView}
+        exact
+        layout={MinimalLayout}
+        path="/not-found"
+      />
+      <Redirect to="/not-found" />
+    </Switch>
+  );
+};
+
+export default Routes;
