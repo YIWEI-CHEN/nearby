31dc2dcb6aceb6c63779c4e3abcb3792a9ec687b
lhenry15 <henry@Henryde-MacBook-Pro.local>
2019-11-10 18:44:25 -0600

    add current location, infowindow

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -1,93 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import { withRouter } from 'react-router-dom';
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker
+  Marker,
+  InfoWindow
 } from "react-google-maps";
 
-const GOOGLE_API_KEY = 'Your Key';
+import { compose, withProps, withStateHandlers } from "recompose";
+const GOOGLE_API_KEY = 'AIzaSyAGWkTVfH4RA2sRbFN9dY489Q-T1At2Fqk';
 const google_map_URL = "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY;
 
-const CustomSkinMap = withScriptjs(
-  withGoogleMap(() => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        zoomControl: true,
-        styles: [
-          {
-            featureType: "water",
-            stylers: [
-              { saturation: 43 },
-              { lightness: -11 },
-              { hue: "#0088ff" }
-            ]
-          },
-          {
-            featureType: "road",
-            elementType: "geometry.fill",
-            stylers: [
-              { hue: "#ff0000" },
-              { saturation: -100 },
-              { lightness: 99 }
-            ]
-          },
-          {
-            featureType: "road",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#808080" }, { lightness: 54 }]
-          },
-          {
-            featureType: "landscape.man_made",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ece2d9" }]
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ccdca1" }]
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#767676" }]
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.stroke",
-            stylers: [{ color: "#ffffff" }]
-          },
-          { featureType: "poi", stylers: [{ visibility: "off" }] },
-          {
-            featureType: "landscape.natural",
-            elementType: "geometry.fill",
-            stylers: [{ visibility: "on" }, { color: "#b8cb93" }]
-          },
-          { featureType: "poi.park", stylers: [{ visibility: "on" }] },
-          {
-            featureType: "poi.sports_complex",
-            stylers: [{ visibility: "on" }]
-          },
-          { featureType: "poi.medical", stylers: [{ visibility: "on" }] },
-          {
-            featureType: "poi.business",
-            stylers: [{ visibility: "simplified" }]
-          }
-        ]
-      }}
+function handleClick(lat, lng){
+  console.log(lat, lng);
+}
+
+
+function getCurLoc(){
+ navigator.geolocation.getCurrentPosition(function(p){
+     localStorage.setItem("lat", p.coords.latitude)
+     localStorage.setItem("lng", p.coords.longitude)
+    })
+}
+
+const MapWithAMakredInfoWindow = compose(
+  withStateHandlers(() => ({
+    currentLoc: {lat: Math.round(localStorage.lat*1000)/1000, lng: Math.round(localStorage.lng*1000)/1000},
+    isOpen: false
+    
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={13}
+    defaultCenter={{ lat: props.currentLoc.lat, lng: props.currentLoc.lng }}
+  >
+    <Marker
+      position={{ lat: props.currentLoc.lat, lng: props.currentLoc.lng}}
+      onClick={props.onToggleOpen}
     >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
+      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+        <p> Care Needed!! </p>
+      </InfoWindow>}
+    </Marker>
+
+  </GoogleMap>
 );
 
 function Maps() {
+  getCurLoc()
   return (
-    <CustomSkinMap
+    <MapWithAMakredInfoWindow
       googleMapURL={google_map_URL}
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `100vh` }} />}
