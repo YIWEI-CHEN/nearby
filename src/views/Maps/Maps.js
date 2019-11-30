28c4c269cb0035fe08be19f1ec3248d6bde7a280
lhenry15 <henry@Henryde-MacBook-Pro.local>
2019-11-30 03:57:24 -0600

    separate markers, add markers infowindow, add fake data, add filter\(debugging\)

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -1,68 +1,155 @@
-import React, { Component } from "react";
-import { withRouter } from 'react-router-dom';
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-  InfoWindow
-} from "react-google-maps";
+import React, { Component }  from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import mockData from './data';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 
-import { compose, withProps, withStateHandlers } from "recompose";
+
 const GOOGLE_API_KEY = 'AIzaSyAGWkTVfH4RA2sRbFN9dY489Q-T1At2Fqk';
 const google_map_URL = "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY;
 
-function handleClick(lat, lng){
-  console.log(lat, lng);
-}
+var data = mockData
 
+class Canvas extends Component {
+  state = {
+      activeMarker: {},
+      selectedPlace: {},
+      showingInfoWindow: false,
+      initialCenter: {
+        lat: 30.601389,
+        lng: -96.314445
+      },
+      checked_Eng: false,
+      checked_Spn: false,
+      checked_Chn: false,
+      checked_Kor: false,
+    };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+  });
+  onInfoWindowClose = () =>
+    this.setState({
+      activeMarker: null,
+      showingInfoWindow: false
+    });
 
-function getCurLoc(){
- navigator.geolocation.getCurrentPosition(function(p){
-     localStorage.setItem("lat", p.coords.latitude)
-     localStorage.setItem("lng", p.coords.longitude)
+  onMapClicked = () => {
+    console.log(mockData)
+    for (var i=0; i<mockData.length; i++) {
+      console.log(mockData[i].name);
+      console.log(mockData[i].location.lat);
+    }
+  };
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.checked
     })
+    // filter the markers
+    let newSelectedUsers = [];
+    data = mockData;
+    for (var i=0; i<mockData.length; i++){
+      var langs = data[i].language.split(", ")
+      if (langs.includes(event.target.value)){
+        data.splice(i,1);
+        // TODO: disable the markers
+      }
+    }
+  };
+
+  render() {
+    var langs = []
+    console.log(this.state);
+    if (!this.props.loaded) return <div>Loading...</div>;
+    return (
+      <Map
+        className="map"
+        google={this.props.google}
+        onClick={this.onMapClicked}
+        initialCenter={this.state.initialCenter}
+        style={{ height: '90%', position: 'relative', width: '100%', up: '10px' }}
+        zoom={13}>
+        {data.map(m => <Marker 
+                    name={m.name} 
+                    position={m.location}
+                    language={m.language}
+                    sex={m.sex}
+                    age={m.age}
+                    onClick={this.onMarkerClick} 
+                    />)}
+
+        <div position="relative" left="30px" up="10px">
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={this.state.checked_Eng}
+                onChange={this.handleChange('checked_Eng')}
+                value="English"
+                color="primary"
+              />
+            }
+            label="English"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={this.state.checked_Spn}
+                onChange={this.handleChange('checked_Spn')}
+                value="Spanish"
+                color="primary"
+              />
+            }
+            label="Spanish"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={this.state.checked_Chn}
+                onChange={this.handleChange('checked_Chn')}
+                value="Chinese"
+                color="primary"
+              />
+            }
+            label="Chinese"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={this.state.checked_Kor}
+                onChange={this.handleChange('checked_Kor')}
+                value="Korean"
+                color="primary"
+              />
+            }
+            label="Korean"
+          />
+        </div>
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          onClose={this.onInfoWindowClose}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h2 align='center'>{this.state.selectedPlace.name}</h2>
+            <li>Language: {this.state.selectedPlace.language}</li>
+            <li>Sex: {this.state.selectedPlace.sex}</li>
+            <li>Age: {this.state.selectedPlace.age}</li>
+            <a align='center' href='test'>Select</a>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
 
-const MapWithAMakredInfoWindow = compose(
-  withStateHandlers(() => ({
-    currentLoc: {lat: Math.round(localStorage.lat*1000)/1000, lng: Math.round(localStorage.lng*1000)/1000},
-    isOpen: false
-    
-  }), {
-    onToggleOpen: ({ isOpen }) => () => ({
-      isOpen: !isOpen,
-    })
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props =>
-  <GoogleMap
-    defaultZoom={13}
-    defaultCenter={{ lat: props.currentLoc.lat, lng: props.currentLoc.lng }}
-  >
-    <Marker
-      position={{ lat: props.currentLoc.lat, lng: props.currentLoc.lng}}
-      onClick={props.onToggleOpen}
-    >
-      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
-        <p> Care Needed!! </p>
-      </InfoWindow>}
-    </Marker>
-
-  </GoogleMap>
-);
-
-function Maps() {
-  getCurLoc()
-  return (
-    <MapWithAMakredInfoWindow
-      googleMapURL={google_map_URL}
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `100vh` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
-  );
-}
-
-export default withRouter(Maps);
+export default GoogleApiWrapper({
+  apiKey: (GOOGLE_API_KEY)
+})(Canvas)
---
+++ src/views/Maps/data.js
@@ -0,0 +1,129 @@
+import uuid from 'uuid/v1';
+
+export default [
+  {
+    id: uuid(),
+    name: 'Ekaterina Tankova',
+    address: {
+      country: 'USA',
+      state: 'West Virginia',
+      city: 'Parkersburg',
+      street: '2849 Fulton Street'
+    },
+    language: 'English, Spanish',
+    sex: 'Female',
+    age: '18',
+    location: {lat:30.601389, lng:-96.314445},
+    email: 'ekaterina.tankova@devias.io',
+    phone: '304-428-3097',
+    avatarUrl: '/images/avatars/avatar_3.png',
+    createdAt: 1555016400000
+  },
+  {
+    id: uuid(),
+    name: 'Cao Yu',
+    address: {
+      country: 'USA',
+      state: 'Bristow',
+      city: 'Iowa',
+      street: '1865  Pleasant Hill Road'
+    },
+    language: 'English, Chinese',
+    sex: 'Male',
+    age: '25',
+    location: {lat:30.611389, lng:-96.344445},
+    email: 'cao.yu@devias.io',
+    avatarUrl: '/images/avatars/avatar_4.png',
+    phone: '712-351-5711',
+    createdAt: 1555016400000
+  },
+  {
+    id: uuid(),
+    name: 'Alexa Richardson',
+    address: {
+      country: 'USA',
+      state: 'Georgia',
+      city: 'Atlanta',
+      street: '4894  Lakeland Park Drive'
+    },
+    language: 'English, Spanish',
+    sex: 'Female',
+    age: '23',  
+    location: {lat:30.641389, lng:-96.364445},
+    email: 'alexa.richardson@devias.io',
+    phone: '770-635-2682',
+    avatarUrl: '/images/avatars/avatar_2.png',
+    createdAt: 1555016400000
+  },
+  {
+    id: uuid(),
+    name: 'Anje Keizer',
+    address: {
+      country: 'USA',
+      state: 'Ohio',
+      city: 'Dover',
+      street: '4158  Hedge Street'
+    },
+    language: 'Spanish, English',
+    sex: 'Female',
+    age: '27',   
+    location: {lat:30.691389, lng:-96.304445},
+    email: 'anje.keizer@devias.io',
+    avatarUrl: '/images/avatars/avatar_5.png',
+    phone: '908-691-3242',
+    createdAt: 1554930000000
+  },
+  {
+    id: uuid(),
+    name: 'Emilee Simchenko',
+    address: {
+      country: 'USA',
+      state: 'Nevada',
+      city: 'Las Vegas',
+      street: '1798  Hickory Ridge Drive'
+    },
+    language: 'English, Spanish',
+    sex: 'Female',
+    age: '31',  
+    location: {lat:30.701389, lng:-96.284445},
+    email: 'emilee.simchenko@devias.io',
+    phone: '702-661-1654',
+    avatarUrl: '/images/avatars/avatar_8.png',
+    createdAt: 1523048400000
+  },
+  {
+    id: uuid(),
+    name: 'Kwak Seong-Min',
+    address: {
+      country: 'USA',
+      state: 'Michigan',
+      city: 'Detroit',
+      street: '3934  Wildrose Lane'
+    },
+    language: 'Korean, English',
+    sex: 'Male',
+    age: '37',  
+    location: {lat:30.571389, lng:-96.354445},
+    email: 'kwak.seong.min@devias.io',
+    avatarUrl: '/images/avatars/avatar_9.png',
+    phone: '313-812-8947'
+  },
+  {
+    id: uuid(),
+    name: 'Clarke Gillebert',
+    address: {
+      country: 'USA',
+      state: 'Texas',
+      city: 'Dallas',
+      street: '75247'
+    },
+    language: 'English, Chinese',
+    sex: 'Male',
+    age: '28', 
+    location: {lat:30.641389, lng:-96.324445},
+    email: 'clarke.gillebert@devias.io',
+    phone: '972-333-4106',
+    avatarUrl: '/images/avatars/avatar_6.png',
+    createdAt: 1554757200000
+  },
+];
